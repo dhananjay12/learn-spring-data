0fbfe3cbf5f6497903e129522f0f6258e8c05434
Dhananjay Singh <dhananjay1204@gmail.com>
2019-01-07 23:41:51 +0530

    spring data mongodb

---
+++ mongodb/src/main/java/com/mynotes/spring/data/mongodb/MongodbApplication.java
@@ -0,0 +1,14 @@
+package com.mynotes.spring.data.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbApplication.class, args);
+	}
+
+}
+
